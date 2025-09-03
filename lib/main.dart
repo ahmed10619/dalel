6cf4745386160a606cc39bdf17e5c85641191de4
Ahmed <ahmed10619@example.com>
2025-09-03 23:26:52 +0300

    add  firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@
 import 'package:dalel/core/routers/app_routers.dart';
 import 'package:dalel/core/services/service_locator.dart';
 import 'package:dalel/core/utils/app_colors.dart';
+import 'package:dalel/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/database/cache/cache_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   seviceSetUp();
   await getIt<CacheHelper>().init();
 
